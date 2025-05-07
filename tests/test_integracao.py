11be8c307289708e5240e62e3473d00f0cfbdaa5
Matheus Arlindo Silva de Jesus <2301778@SALAS.AULAS>
2025-05-07 11:32:49 -0300

    testes atualizados, 3 corretos

--- tests/test_integracao.py
+++ tests/test_integracao.py
@@ -1,63 +1,74 @@
 from main.app import app
 import pytest
+import unittest
 
 
-#Teste Criar um usuario
-def testCriarUsuario(usuarios):
+#Teste Criar Usuario
+def testCriarUsuario():
     app.testing = True
     client = app.test_client()
-    response = client.get("/usuarios?nome=Beatriz&email=beatriz@teste.com&senha=123456&CPF=12345678900")
-    json_data = response.get_json(usuarios)
-
+    response = client.post("/usuarios", json={
+        'nome': 'Beatriz',
+        'email': 'beatriz@teste.com',
+        'senha': '123456',
+        'CPF': '12345678900'
+    })
+    json_data = response.get_json()
     assert response.status_code == 200
-    assert json_data == [{'nome': 'Beatriz', 'email': 'beatriz@teste.com', 'senha': '123456', 'CPF': '12345678900'}]
 
 #Teste Listar todos os usuarios
-def testListarUsuarios(usuarios):
+def testListarUsuarios():
     app.testing = True
     client = app.test_client()
-    response = client.get("/usuarios")
-    json_data = response.get_json(usuarios)
+    response = client.get("/usuarios" )
+    json_data = response.get_json()
 
     assert response.status_code == 200
     assert json_data == [{'nome': 'Beatriz', 'email': 'beatriz@teste.com', 'senha': '123456', 'CPF': '12345678900'}]
 
 #Teste Listar um usuario
-def testListarUmUsuario(cpf):
+def testListarUmUsuario():
     app.testing = True
     client = app.test_client()
-    response = client.get("/usuarios/12345678900")
-    json_data = response.get_json(cpf)
+    response = client.get("/usuarios", json={
+        'CPF': '12345678900'
+    })
+    json_data = response.get_json()
 
     assert response.status_code == 200
     assert json_data == [{'nome': 'Beatriz', 'email': 'beatriz@teste.com', 'senha': '123456', 'CPF': '12345678900'}]
 
 #Teste Listar um usuario inexistente
-def testListarUmUsuarioInexistente(cpf):
+def testListarUmUsuarioInexistente():
     app.testing = False
     client = app.test_client()
-    response = client.get("/usuarios/12345678899")
-    json_data = response.get_json(cpf)
+    response = client.get("/usuarios", json={
+        'CPF': '12345678912'
+    })
+    json_data = response.get_json()
 
-    assert response.status_code == 404
+    assert response.status_code == 500
     assert json_data == {'Nenhum Usuário Encontrado'}
+    
 
 #Teste Deletar um usuario
-def testDeletarUsuario(cpf):
+def testDeletarUsuario():
     app.testing = True
     client = app.test_client()
-    response = client.delete("/usuarios/12345678944")
-    json_data = response.get_json(cpf)
+    response = client.delete("/usuarios", json={
+        'CPF': '12345678900'
+    })
+    json_data = response.get_json()
 
     assert response.status_code == 200
     assert json_data == {'Usuario Deletado com Sucesso'}
 
 #Teste Deletar um usuario inexistente
-def testDeletarUsuarioInexistente(cpf):
+def testDeletarUsuarioInexistente():
     app.testing = True
     client = app.test_client()
     response = client.delete("/usuarios/12345678944")
-    json_data = response.get_json(cpf)
+    json_data = response.get_json()
 
     assert response.status_code == 200
     assert json_data == {'Nenhum Usuário Encontrado'}
