7eea8f5539998b7c763f7d13b4e778622ad8a528
Matheus Arlindo Silva de Jesus <2301778@SALAS.AULAS>
2025-05-14 10:47:49 -0300

    testes passaram

--- tests/test_integracao.py
+++ tests/test_integracao.py
@@ -3,71 +3,75 @@ import pytest
 import unittest
 
 
-#Teste Criar Usuario
-def testCriarUsuario():
-    app.testing = True
-    client = app.test_client()
-    response = client.post("/usuarios", json={
-        'nome': 'Beatriz',
-        'email': 'beatriz@teste.com',
-        'senha': '123456',
-        'CPF': '12345678900'
-    })
-    json_data = response.get_json()
-    assert response.status_code == 200
-    assert json_data == {'nome': 'Beatriz', 'email': 'beatriz@teste.com', 'senha': '123456', 'CPF': '12345678900'}
+class TestUsuario(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.client = app.test_client()
 
-#Teste Listar todos os usuarios
-def testListarUsuarios():
-    app.testing = True
-    client = app.test_client()
-    response = client.get("/usuarios" )
-    json_data = response.get_json()
+    #Teste Criar Usuario
+    def testCriarUsuario(self):
+        response = self.client.post("/usuarios", json={
+            'nome': 'Beatriz',
+            'email': 'beatriz@teste.com',
+            'senha': '123456',
+            'CPF': '12345678900'
+        })
+        json_data = response.get_json()
+        self.assertEqual (response.status_code, 200)
+        self.assertEqual (json_data, {'nome': 'Beatriz', 
+                                      'email': 'beatriz@teste.com', 
+                                      'senha': '123456', 
+                                      'CPF': '12345678900'
+                                        })  
 
-    assert response.status_code == 200
-    assert json_data == [{'nome': 'Beatriz', 'email': 'beatriz@teste.com', 'senha': '123456', 'CPF': '12345678900'}]
+    #Teste Listar todos os usuarios
+    def testListarUsuarios(self):
+        
+        response = self.client.get("/usuarios" )
+        json_data = response.get_json()
 
-#Teste Listar um usuario
-def testListarUmUsuario():
-    app.testing = True
-    client = app.test_client()
-    response = client.get("/usuarios", json={
-        'CPF': '12345678900'
-    })
-    json_data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual (json_data, [{'nome': 'Beatriz', 'email': 'beatriz@teste.com', 'senha': '123456', 'CPF': '12345678900'}])
 
-    assert response.status_code == 200
-    assert json_data == [{'nome': 'Beatriz', 'email': 'beatriz@teste.com', 'senha': '123456', 'CPF': '12345678900'}]
+    #Teste Listar um usuario
+    def testListarUmUsuario(self):
+        # Cria o usuário primeiro
+        self.client.post("/usuarios", json={
+            'nome': 'Beatriz',
+            'email': 'beatriz@teste.com',
+            'senha': '123456',
+            'CPF': '12345678900'
+            })
 
-#Teste Listar um usuario inexistente
-def testListarUmUsuarioInexistente():
-    app.testing = True
-    client = app.test_client()
-    response = client.get("/usuarios/12345678999")
-    json_data = response.get_json() 
+        response = self.client.get("/usuarios/12345678900")
+        json_data = response.get_json()
 
-    assert response.status_code == 200
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_data, {'nome': 'Beatriz', 'email': 'beatriz@teste.com', 'senha': '123456', 'CPF': '12345678900'})
+
+    #Teste Listar um usuario inexistente
+    def testListarUmUsuarioInexistente(self):
+        response = self.client.get("/usuarios/12345678999")
+        json_data = response.get_json() 
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_data['mensagem'], 'Nenhum Usuário Encontrado')
+
+    #Teste Deletar um usuario
+    def testDeletarUsuario(self):
+        response = self.client.delete("/usuarios/12345678900")
+        json_data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual (json_data['mensagem'], 'Usuario Deletado com Sucesso')
+
+    #Teste Deletar um usuario inexistente
+    def testDeletarUsuarioInexistente(self):
+        response = self.client.delete("/usuarios/12345678999")
+        json_data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_data['mensagem'], 'Nenhum Usuário Encontrado')
     
-    assert json_data['mensagem'] == 'Nenhum Usuário Encontrado'
-
-#Teste Deletar um usuario
-def testDeletarUsuario():
-    app.testing = True
-    client = app.test_client()
-    response = client.delete("/usuarios/12345678900")
-    json_data = response.get_json()
-
-    assert response.status_code == 200
-    assert json_data['mensagem'] == 'Usuario Deletado com Sucesso'
-
-#Teste Deletar um usuario inexistente
-def testDeletarUsuarioInexistente():
-    app.testing = True
-    client = app.test_client()
-    response = client.delete("/usuarios/12345678999")
-    json_data = response.get_json()
-
-    assert response.status_code == 200
-    assert json_data['mensagem'] == 'Nenhum Usuário Encontrado'
-    
-
+if __name__ == '__main__':
+    unittest.main()
